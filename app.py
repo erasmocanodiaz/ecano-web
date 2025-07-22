50219ad16991219313e62e00266ca65420e557bf
erasmo cano <34849064+erasmocanodiaz@users.noreply.github.com>
2025-07-22 06:08:56 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-# Ruta principal
 @app.route('/')
 def home():
-    return render_template_string("""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Bienvenido a Ecanosoft</title>
-        <style>
-            body { font-family: Arial; background: #f3f3f3; text-align: center; padding-top: 100px; }
-            h1 { color: #333; }
-        </style>
-    </head>
-    <body>
-        <h1>¡Bienvenido a tu app Flask en Railway, Erasmo!</h1>
-        <p>Funciona perfecto con tu dominio personalizado.</p>
-    </body>
-    </html>
-    """)
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Railway asigna el puerto por variable de entorno
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
